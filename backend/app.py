ff643965b2c7c0684c900bf4a0746b8af956b2ab
Muhammad Arslan Nawaz <muhammadarslannawaz@Muhammads-MacBook-Pro.local>
2026-05-28 18:22:10 +0500

    Prepared backend for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,55 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import ollama
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/explain', methods=['POST'])
-def explain_sql():
+def explain():
 
-    try:
-
-        data = request.json
-
-        sql_query = data.get('query')
-
-        prompt = f"""
-        Explain this SQL query in simple English:
-        Include:
-        1. What the query does
-        2. Which table is used
-        3. Conditions applied
-        4. Simple explanation for beginners
-        SQL Query:
-        {sql_query}
-        """
-
-        response = ollama.chat(
-
-            model='phi3',
-
-            messages=[
-                {
-                    'role': 'user',
-                    'content': prompt
-                }
-            ]
-        )
-
-        explanation = response['message']['content']
-
-        return jsonify({
-            "explanation": explanation
-        })
-
-    except Exception as e:
-
-        print("ERROR:", str(e))
-
-        return jsonify({
-            "error": str(e)
-        }), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return jsonify({
+        "explanation": "Backend deployment successful!"
+    })
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
     try {
 
       const response = await fetch(
-        "http://127.0.0.1:5000/explain",
+        "http://127.0.0.1:5001/explain",
         {
           method: "POST",
 
