4a1f613ad9502b86ca8da4311c555f0de5718a18
Muhammad Arslan Nawaz <muhammadarslannawaz@Muhammads-MacBook-Pro.local>
2026-06-03 16:42:57 +0500

    Enabled OpenRouter

--- app.py
+++ app.py
@@ -6,10 +6,53 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 @app.route('/explain', methods=['POST'])
 def explain():
 
-    return jsonify({
-        "explanation": "Railway backend reached successfully"
-    })
+    try:
+
+        data = request.get_json()
+        sql_query = data.get("query", "")
+
+        prompt = f"""
+Explain this SQL query in simple language.
+
+SQL Query:
+{sql_query}
+"""
+
+        response = requests.post(
+            "https://openrouter.ai/api/v1/chat/completions",
+            headers={
+                "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "model": "google/gemma-3-4b-it:free",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                ]
+            }
+        )
+
+        result = response.json()
+
+        print(result)
+
+        if "choices" not in result:
+            return jsonify({
+                "explanation": str(result)
+            })
+
+        return jsonify({
+            "explanation": result["choices"][0]["message"]["content"]
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "explanation": f"Error: {str(e)}"
+        })
